f8244fa89959315712856dfe3d67c1addf0370f6
rmartinezfernandez <rmartinezfernandez@ITEM-80413.emea.msad.sopra>
2018-03-19 17:52:37 +0100

    primer commit del proyecto

---
+++ src/pkg12/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg12;
+
+/**
+ *
+ * @author rmartinezfernandez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+		System.out.println("ESTE ES MI SEGUNDO PROGRAMA JAVA");
+    }
+    
+}
